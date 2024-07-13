106dcb6113b48e0c8a6d75c61dc636f3472d63cb
reopenpilot <153556801+reopenpilot@users.noreply.github.com>
2024-07-13 14:53:58 +0800

    Create openpilot-tuner.sh

---
+++ openpilot-tuner.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+: '
+Skip steps 1 & 2 if not using Telegram notifications.
+
+1. Get a Telegram bot token & chat_id:
+https://gist.github.com/nafiesl/4ad622f344cd1dc3bb1ecbe468ff9f8a
+
+2. Replace ___TOKEN___ and ___CHAT_ID___ in the commands below with your token & chat_id. Run these in OpenPilot SSH:
+
+echo "___TOKEN___" > /data/params/d/ZzTelegramToken
+echo "___CHAT_ID___" > /data/params/d/ZzTelegramChatID
+
+3. Run this command in OpenPilot SSH:
+curl -fsSL https://gist.githubusercontent.com/reopenpilot/openpilot-tuner/raw/openpilot-tuner.sh | sudo tee /data/continue.sh > /dev/null
+'
+
+TOKEN=$(cat /data/params/d/ZzTelegramToken 2>/dev/null || echo "")
+CHAT_ID=$(cat /data/params/d/ZzTelegramChatID 2>/dev/null || echo "")
+TELEGRAM_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+
+send_telegram() {
+    local message="$1"
+    local start_time=$(date +%s)
+    if [ -z "$TOKEN" ] || [ -z "$CHAT_ID" ]; then
+        echo "TOKEN or CHAT_ID is an empty string, skiping this function."
+        return
+    fi
+    (
+        while true; do
+            if curl -s -X POST $TELEGRAM_URL -d chat_id=$CHAT_ID -d parse_mode="MarkdownV2" -d text="$message" > /dev/null; then
+                break
+            fi
+            local now=$(date +%s)
+            if [ $((now - start_time)) -ge 3600 ]; then
+                echo "Failed to send message after 1 hour. Abandoning attempts."
+                break
+            fi
+            sleep 30
+        done
+    ) &
+}
+
+cd /data/openpilot
+cd /data/safe_staging/finalized
+
+filename_athenad=system/athena/athenad.py
+filename_powermonitoring=system/hardware/power_monitoring.py
+
+errors=""
+
+{
+    git checkout -- $filename_athenad 2>&1
+    git checkout -- $filename_powermonitoring 2>&1
+    find .git -exec touch -t 200001010000 {} \; 2>&1
+
+    sed -i "s/if len(file.fn) == 0/if 'dcamera' in file.fn or len(file.fn) == 0/" $filename_athenad 2>&1
+    sed -i 's/VBATT_PAUSE_CHARGING = 11.8/VBATT_PAUSE_CHARGING = 11.2/' $filename_powermonitoring 2>&1
+    sed -i 's/should_shutdown |= (self.car_battery_capacity_uWh <= 0)/#should_shutdown |= (self.car_battery_capacity_uWh <= 0)/' $filename_powermonitoring 2>&1
+} 2>&1 | {
+    while IFS= read -r line; do
+        errors+="$line\n"
+    done
+}
+
+changes=$(git diff --no-prefix --minimal --unified=0 | sed 's/`/\\`/g')
+changes=${changes:0:2000}
+errors=${errors:0:2000}
+
+send_telegram "\`\`\`
+#ERROR:
+$errors
+\`\`\`"
+
+send_telegram "\`\`\`
+#CHANGES:
+$changes
+\`\`\`"
+
+cd /data/openpilot
+exec ./launch_openpilot.sh
